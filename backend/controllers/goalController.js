6e5a10892ba19c913d68f1a7e763c663c4c0c854
hyperkarmic <randy_pan@yahoo.com>
2022-02-08 22:34:01 +0000

    mongoose models added - full CRUD functionality attained

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler')
-
+const Goal = require('../models/goalModel')
 // 'desc   Get Goals
 // @route Get api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get goals' })
+  const goals = await Goal.find()
+  res.status(200).json(goals)
 })
 
 //@desc Set Goal
@@ -17,7 +18,10 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('text field - where???')
   }
-  res.status(200).json({ message: 'Set goal' })
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+  res.status(200).json(goal)
 })
 
 //@desc update Goal
@@ -25,14 +29,29 @@ const setGoal = asyncHandler(async (req, res) => {
 //@ access = Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goals ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+  console.log(updatedGoal)
+  res.status(200).json(updatedGoal)
 })
 //@desc delete Goal
 //@route DELETE /api/goals
 //@ access = Private
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goals ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  await goal.remove()
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
